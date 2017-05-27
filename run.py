5c6ce52980652285563dd8f52e7ea0a69d33a09e
cy-dream <18713823671@163.com>
2017-05-27 18:12:59 +0800

    add logger

--- run.py
+++ run.py
@@ -10,17 +10,26 @@ import json
 import tail
 import yaml
 import requests
+import logging
+
+logger = logging.getLogger("logMonitor")
+logger.setLevel(logging.INFO)
+filehandler = logging.FileHandler('log_monitor.log','w')
+formatter =  logging.Formatter('%(asctime)s  %(filename)s  %(levelname)s - %(message)s',datefmt='%a, %d %b %Y %H:%M:%S',)
+filehandler.setFormatter(formatter)
+logger.addHandler(filehandler)
 
 with open('config.yml') as f:
   config = yaml.load(f)
 
 def return_line(msg):
   ''' Prints received text '''
+  logger.debug('Wechat send message [{}] \n'.format(msg.strip()))
   for pattern_item in config['sending_patterns']:
     if [x for x in pattern_item['stop_words'] if x in msg]:
       if len(msg) > config['msglength']:
         msg = msg[0:config['msglength']]
-      send_msg(msg, pattern_item['recipients'])
+      send_msg(msg.strip(), pattern_item['recipients'])
    
 def send_msg(msg, recipients):
   values = {'recipients' : recipients,
@@ -30,9 +39,9 @@ def send_msg(msg, recipients):
   headers = {'Content-Type': 'application/json'}
   data = json.dumps(values)
   req = requests.post(url, data, headers=headers)
+  logger.info('Wechat send status {}, content {}'.format(req, req.text))
 
-if __name__ == "__main__":
-  t = tail.Tail(config['logpath'])
-  t.register_callback(return_line)
-  t.follow(s=1)
 
+t = tail.Tail(config['logpath'])
+t.register_callback(return_line)
+t.follow(s=1)
